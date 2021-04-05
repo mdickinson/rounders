27bd377bb3c3201bb528eb0923c4201cc9627f70
Mark Dickinson <dickinsm@gmail.com>
2021-04-05 10:32:43 +0100

    Refactoring

--- rounder/modes.py
+++ rounder/modes.py
@@ -20,13 +20,20 @@ TO_ODD = [[3, 0], [3, 0]]
 TO_ZERO_05_AWAY = "TO_ZERO_05_AWAY"
 
 
+def round_decide(quarters: SignedQuarterInt, *, rounding_mode) -> SignedInt:
+    """
+    Decide whether to round a given value up or not, for a given rounding mode.
+    """
+    if rounding_mode == TO_ZERO_05_AWAY:
+        return quarters.quarters > 0 and quarters.whole % 5 == 0
+    else:
+        odd = quarters.whole & 1
+        return quarters.quarters + rounding_mode[quarters.sign][odd] >= 4
+
+
 def round_quarters(quarters: SignedQuarterInt, *, rounding_mode) -> SignedInt:
     """
     Round a quarter-integer to an integer using the given rounding mode.
     """
-    if rounding_mode == TO_ZERO_05_AWAY:
-        round_up = quarters.quarters > 0 and quarters.whole % 5 == 0
-    else:
-        odd = quarters.whole & 1
-        round_up = quarters.quarters + rounding_mode[quarters.sign][odd] >= 4
-    return SignedInt(quarters.sign, quarters.whole + round_up)
+    round_away = round_decide(quarters, rounding_mode=rounding_mode)
+    return SignedInt(quarters.sign, quarters.whole + round_away)
