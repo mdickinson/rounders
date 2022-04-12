271e0d4f70bcc6b7ad45591fca6cfab8c78077dd
Mark Dickinson <dickinsm@gmail.com>
2022-04-12 15:26:40 +0100

    Add docstring for rounding modes

--- rounder/modes.py
+++ rounder/modes.py
@@ -4,6 +4,52 @@ Rounding modes.
 A rounding mode is a callable that accepts a quarter integer value (something
 of type SignedQuarterInt) and returns a bool indicating whether that value should
 be rounded away from zero.
+
+This module implements 13 rounding modes. There are six directed rounding modes,
+six to-nearest rounding modes, and one extra rounding mode TO_ZERO_05_AWAY
+
+Directed rounding modes
+-----------------------
+
+The six directed rounding modes occur in three opposite pairs, here illustrated by
+their actions when rounding a real number to an integer.
+
+To zero: floor(x) for nonnegative x, ceil(x) for negative x
+
+    ⋯→→●→→→●→→→●←←←●←←←●←←←●←←⋯
+      -2  -1   0   1   2   3
+
+
+To away: floor(x) for negative x, ceil(x) for nonnegative x
+
+    ⋯←←●←←←●←←←●→→→●→→→●→→→●→→⋯
+      -2  -1   0   1   2   3
+
+
+To minus: floor(x)
+
+    ⋯←←●←←←●←←←●←←←●←←←●←←←●←←⋯
+      -2  -1   0   1   2   3
+
+To plus: ceil(x)
+
+    ⋯→→●→→→●→→→●→→→●→→→●→→→●→→⋯
+      -2  -1   0   1   2   3
+
+To even: floor(x) if floor(x) is even, else ceil(x)
+
+    ⋯→→●←←←●→→→●←←←●→→→●←←←●→→⋯
+      -2  -1   0   1   2   3
+
+To odd: floor(x) if floor(x) is odd, else ceil(x)
+
+    ⋯←←●→→→●←←←●→→→●←←←●→→→●←←⋯
+      -2  -1   0   1   2   3
+
+There are six corresponding round-to-nearest rounding modes: where every input
+value x rounds to the nearest integer, and the zero / away / minus / plus / even / odd
+rule only applies in halfway cases - numbers of the form n + 0.5 for an integer n.
+
 """
 
 
