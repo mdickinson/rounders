0ca23f5f10408f77d3981c7bdb1c4c0c29c2c075
Mark Dickinson <dickinsm@gmail.com>
2023-01-15 16:56:04 +0000

    Don't make 'format' publicly available yet (#17)
    The `format` function is preliminary; don't expose it at top-level.

--- src/rounder/__init__.py
+++ src/rounder/__init__.py
@@ -6,7 +6,6 @@ import rounder.decimal_overloads  # noqa: F401
 import rounder.float_overloads  # noqa: F401
 import rounder.fraction_overloads  # noqa: F401
 import rounder.int_overloads  # noqa: F401
-from rounder.format import format
 from rounder.mode_specific import (
     round_ties_to_away,
     round_ties_to_even,
@@ -40,14 +39,12 @@ from rounder.modes import (
 from rounder.round_to import round, round_to_figures, round_to_int, round_to_places
 
 __all__ = [
-    # Formatting
-    "format",
     # Top-level rounding functions
     "round",
     "round_to_figures",
     "round_to_int",
     "round_to_places",
-    # Mode-specific rounding functions
+    # Mode-specific rounding functions - replacements for round
     "round_ties_to_away",
     "round_ties_to_even",
     "round_ties_to_minus",
@@ -61,18 +58,20 @@ __all__ = [
     "round_to_plus",
     "round_to_zero",
     "round_to_zero_05_away",
-    # Rounding modes
+    # Rounding modes - to-nearest
     "TIES_TO_AWAY",
     "TIES_TO_EVEN",
     "TIES_TO_MINUS",
     "TIES_TO_ODD",
     "TIES_TO_PLUS",
     "TIES_TO_ZERO",
+    # Rounding modes - directed
     "TO_AWAY",
     "TO_EVEN",
     "TO_MINUS",
     "TO_ODD",
     "TO_PLUS",
     "TO_ZERO",
+    # Round for re-round
     "TO_ZERO_05_AWAY",
 ]
